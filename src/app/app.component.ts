6dd3d38607a8559604ce71e5d738337e69e23c40
Vanessa Ponte <vanessaponte@gmail.com>
2025-05-10 11:29:48 -0300

    adicionado componente de receitas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./shared/ui/header/header.component";
 import { RecipeService } from './recipe/data/recipe.service';
+import { RecipesComponent } from './recipe/feature/recipes/recipes.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RecipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'My Recipe Book';
-
-  // somente para o teste da API
-  constructor(private service: RecipeService) {
-    this.service.get().subscribe((data) => {
-      console.log(data);
-    })
-  }
 }
