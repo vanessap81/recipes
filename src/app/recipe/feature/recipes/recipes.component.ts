dcb64322dc17c2bfecd83b9a5f7429606ed04058
Vanessa Ponte <vanessaponte@gmail.com>
2025-05-10 16:08:42 -0300

    inserida navegação em recipes

--- src/app/recipe/feature/recipes/recipes.component.ts
+++ src/app/recipe/feature/recipes/recipes.component.ts
@@ -3,6 +3,7 @@ import { RecipeService } from '../../data/recipe.service';
 import { CategoryService } from '../../data/category.service';
 import { AsyncPipe } from '@angular/common';
 import { RecipeGridComponent } from "../../ui/recipe-grid/recipe-grid.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
@@ -13,7 +14,11 @@ import { RecipeGridComponent } from "../../ui/recipe-grid/recipe-grid.component"
 export class RecipesComponent {
   recipeService = inject(RecipeService);
   categoryService = inject(CategoryService);
+  router = inject(Router);
 
   recipes$ = this.recipeService.get();
   categories$ = this.categoryService.get();
+  goToRecipe(id:string){
+    this.router.navigate(['/receitas', id]);
+  }
 }
