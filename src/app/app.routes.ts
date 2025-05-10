19301e88503d9f62bb073c773815529e58bdef5a
Vanessa Ponte <vanessaponte@gmail.com>
2025-05-10 16:00:49 -0300

    definição de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipe/feature/recipes/recipes.component';
+import { RecipeComponent } from './recipe/feature/recipe/recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: RecipesComponent},
+    {path: 'receitas/:id', component: RecipeComponent},
+    {path: '**', redirectTo: 'home'}
+];
