fed32a64d39bfcd6812d7b6949bc00f98df98d0d
Vanessa Ponte <vanessaponte@gmail.com>
2025-05-10 16:31:41 -0300

    implementando lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { RecipesComponent } from './recipe/feature/recipes/recipes.component';
-import { RecipeComponent } from './recipe/feature/recipe/recipe.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: RecipesComponent},
-    {path: 'receitas/:id', component: RecipeComponent},
+    {
+        path: 'receitas/:id', 
+        loadComponent: () => 
+            import('./recipe/feature/recipe/recipe.component').then(
+                (c) => c.RecipeComponent,
+            )
+    },
     {path: '**', redirectTo: 'home'}
 ];
